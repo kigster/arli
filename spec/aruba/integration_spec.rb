09d732325b9ad0abc07dceb050ebd720eb4380cf
Konstantin Gredeskoul <kigster@gmail.com>
2017-11-26 01:40:45 -0800

    Adding Aruba tests

---
+++ spec/aruba/integration_spec.rb
@@ -0,0 +1,23 @@
+require 'aruba_helper'
+
+RSpec.describe 'arli executable', :type => :aruba do
+
+  let(:command) { "exe/arli #{args}" }
+  let(:output) { last_command_started.stdout.chomp }
+
+  before { run_simple command }
+
+  context 'help' do
+    let(:args) { '-h' }
+    it 'should print help' do
+      expect(output).to match(/Arli/)
+    end
+  end
+
+  context 'search help' do
+    let(:args) { 'search -h' }
+    it 'should print search helps' do
+      expect(output).to match(/name-match/)
+    end
+  end
+end
--- spec/aruba_helper.rb
+++ spec/aruba_helper.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 require 'aruba/rspec'
+require 'aruba/in_process'
+require 'arli/cli/runner'
 
 RSpec.configure do |config|
+  config.include Aruba::Api
 end
+
+Aruba.configure do |config|
+  config.command_launcher = :in_process
+  config.main_class = Arli::CLI::Runner
+end
+
